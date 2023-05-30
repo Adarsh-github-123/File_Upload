a0d94b105c952a2ce1e142f465e6fa2ecb0f6b6a
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-30 16:33:32 +0530

    auth.js created and handled

---
+++ routes/product.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {isAuthenticated, isSeller} = require('../middlewares/auth');
+
+router.post("/create", isAuthenticated, isSeller, (req, res) => {
+
+})
+
+module.exports = router;
+
